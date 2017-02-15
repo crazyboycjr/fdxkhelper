82eeb887a7dc5b197b631c252424980d94b70d1b
crazyboycjr <crazyboycjr@gmail.com>
2017-02-15 23:59:05 +0800

    the extension finally work!!

--- xk.js
+++ xk.js
@@ -11,7 +11,7 @@ function main() {
 			      <input id="myinput" type="text" placeholder="Course ID">
 			      <button id="startBtn" type="button">开始选课</button>
 			      <button id="stopBtn" type="button">停止选课</button>
-			      <button id="quitBtn" type="button">退课</button>
+			      <!--<button id="quitBtn" type="button">退课</button>-->
 			    </form>
 			    <div id="resultArea" class="well"></div>
 			</div>
@@ -48,43 +48,6 @@ function main() {
 	getConfig();
 	modifyPage();
 	
-	function printState(data) {
-		$('#resultArea').append(`<div class="result"></div>`);
-		let $result = $('.result');
-		let $data = $(data);
-		let date = new Date;
-		let timeString = date.toLocaleTimeString().slice(0, -3) + '.'
-			+ date.getMilliseconds() + date.toLocaleTimeString().slice(-3);
-		$data.find('td').first().before(`<td style="text-align:center;">${timeString}</td>`);
-		$result.last().html($data);
-		let n = $result.size();
-		if (n > 5) $result.first().remove();
-	}
-
-	function selectLoop(lessonId, course, callback) {
-		function doSelect() {
-			let url = '/xk/stdElectCourse!batchOperator.action' + '?' + 'profileId=' + window.profileId;
-			
-			recognize().then((token) => {
-				console.log('The token is:', token);
-				console.log('POSTing...', url);
-				//$.post('/xk/stdElectCourse!batchOperator.action?profileId=404',
-
-				$.post(url, { optype: 'true', operator0: lessonId + ':true:0' , captcha_response: token }, (data) => {
-					printState(data);
-
-					if (data.indexOf('成功') > 0 || data.indexOf('success') > 0) {
-						callback();
-					}
-				});
-			}, (err) => {
-				console.log(err);
-			});
-		}
-		
-		window.intervalId = setInterval(doSelect, 230);
-	}
-	
 	function getCourseId() {
 		return new Promise((resolve, reject) => {
 			let course = $("#myinput").val();
@@ -93,26 +56,32 @@ function main() {
 			if (res.length === 0) {
 				reject('没有找到该课程，请检查选课号。');
 			}
-			resolve(res[0].id, course);
+			resolve([res[0].id, course]);
 		});
 	};
 
 	$('#startBtn').click(() => {
-		getCourseId().then((lessonId, course) => {
-			selectLoop(lessonId, course, () => {
-				clearInterval(window.intervalId);
+		getCourseId().then((arr) => {
+			lessonId = arr[0];
+			course = arr[1];
+			setTimeout(() => {
+				document.dispatchEvent(new CustomEvent('RW759_connectExtension', {
+					detail: {0: lessonId, 1: course, 2: window.profileId, 3: 1}
+				}));
+			}, 0);
 
-				$('#selectHelper').append(`
-					<div class="alert alert-success" role="alert">
-					<strong>Well done!</strong> You successfully select ${course}.
-					</div>
-				`);
-			});
 		}, (errMsg) => {
 			alert(errMsg);
 		});
 	});
-	$('#stopBtn').click(() => { clearInterval(window.intervalId); });
+
+	$('#stopBtn').click(() => {
+		setTimeout(() => {
+			document.dispatchEvent(new CustomEvent('RW759_connectExtension', {
+				detail: {0: null, 1: null, 2: null, 3: 0}
+			}));
+		}, 	0);
+	});
 
 }
 
@@ -125,12 +94,6 @@ document.body.appendChild(script);
 function recognize() {
 	const appId = 'baibnlajdgdfldnolclmgghenajommha';
 	const imgUrl = 'http://xk.fudan.edu.cn/xk/captcha/image.action';
-
-	$(document).ready(() => {
-		$.get(imgUrl, (data) => {
-			console.log(data);
-		});
-	});
 
 	return new Promise((resolve, reject) => {
 
@@ -148,13 +111,71 @@ function recognize() {
 
 		fetchBlob(imgUrl, (blob) => {
 			let imgData = btoa(String.fromCharCode.apply(null, new Uint8Array(blob)));
-			console.log('imgData:', imgData);
+			//console.log('imgData:', imgData);
 			chrome.runtime.sendMessage(appId, {getToken: true, imgData: imgData}, (response) => {
 				console.log('response:', response);
 				resolve(response.token);
 			});
 		});
 	});
-}	
+}
 
-recognize();
+function printState(data) {
+	$('#resultArea').append(`<div class="result"></div>`);
+	let $result = $('.result');
+	let $data = $(data);
+	let date = new Date;
+	let timeString = date.toLocaleTimeString().slice(0, -3) + '.'
+		+ date.getMilliseconds() + date.toLocaleTimeString().slice(-3);
+	$data.find('td').first().before(`<td style="text-align:center;">${timeString}</td>`);
+	$result.last().html($data);
+	let n = $result.size();
+	if (n > 5) $result.first().remove();
+}
+
+function selectLoop(lessonId, course, callback) {
+	function doSelect() {
+		let url = '/xk/stdElectCourse!batchOperator.action' + '?' + 'profileId=' + window.profileId;
+
+		recognize().then((token) => {
+			console.log('The token is:', token);
+			console.log('POSTing...', url);
+			//$.post('/xk/stdElectCourse!batchOperator.action?profileId=404',
+
+			$.post(url, { optype: 'true', operator0: lessonId + ':true:0' , captcha_response: token }, (data) => {
+				printState(data);
+
+				if (data.indexOf('成功') > 0 || data.indexOf('success') > 0) {
+					callback();
+				}
+			});
+		}, (err) => {
+			console.log(err);
+		});
+	}
+
+	window.intervalId = setInterval(doSelect, 230);
+}
+
+document.addEventListener('RW759_connectExtension', (e) => {
+	shouldStart = e.detail[3];
+
+	if (shouldStart) {
+		lessonId = e.detail[0];
+		course = e.detail[1];
+		window.profileId = e.detail[2];
+
+		selectLoop(lessonId, course, () => {
+			clearInterval(window.intervalId);
+			$('#selectHelper').append(`
+				<div class="alert alert-success" role="alert">
+				<strong>Well done!</strong> You successfully select ${course}.
+				</div>
+			`);
+		});
+	} else {
+		clearInterval(window.intervalId);
+	}
+});
+
+//recognize();
